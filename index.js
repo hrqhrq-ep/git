c841d8ccca5257014bce4b49c585efbd156a67ba
giri <giridhargdv7@gmail.com>
2026-03-25 10:10:46 +0530

    g

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-console.log("welocme");
-
-for(var i=0; i<5; i++)
-{
-    console.log("code");
-=======
 console.log("welocme to playlist");
 for(var i =0;i<5;i++){
     console.log("code io")
->>>>>>> main
 }
